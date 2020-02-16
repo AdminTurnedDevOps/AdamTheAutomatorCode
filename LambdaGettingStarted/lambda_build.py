7ef9b78e8f96cd31faf5e00e078d58835c5b6961
Michael Levan <mlevan1992@outlook.com>
2020-02-16 08:54:40 -0500

    Getting started code for Lambda functions

--- LambdaGettingStarted/lambda_build.py
+++ LambdaGettingStarted/lambda_build.py
@@ -2,19 +2,21 @@ import boto3
 import logging
 import sys
 
-def lambda_build(LambdafunctionName, iamRole):
+def lambda_build(LambdaFunctionName, iamRole):
     client = boto3.client('lambda')
 
     create_lambda_function = client.create_function(
-        FunctionName=LambdafunctionName,
+        FunctionName=LambdaFunctionName,
         Runtime='python3.7',
         Role=iamRole,
         Handler='{}.lambda_handler'.format('lambda_build'),
         Description='Start a virtual machine',
-        Code = {'S3Bucket':'clouddev.engineering', 'S3Key': 'start_ec2instance.py'}
+				Code = {'S3Bucket':'name_of_your_s3_bucket', 'S3Key':'file_name_of_zipped_python_code'}
     )
 
-LambdafunctionName = sys.argv[1]
+LambdaFunctionName = sys.argv[1]
 iamRole = sys.argv[2]
 
-lambda_build(LambdafunctionName, iamRole)
+lambda_build(LambdaFunctionName, iamRole)
+
+## python .\lambda_build.py 'name_of_your_lambda_function' 'arn_of_your_lambda_role'
